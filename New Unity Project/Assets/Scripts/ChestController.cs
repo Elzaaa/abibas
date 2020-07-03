ef5a4abddae79819772cb4f19a3246709e7a9c6e
NaraMara <Actuallyschegolkovmatvey@gmail.com>
2020-07-03 14:07:40 +0700

    comfotable chest filling

--- New Unity Project/Assets/Scripts/ChestController.cs
+++ New Unity Project/Assets/Scripts/ChestController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChestController : MonoBehaviour
 {
     public bool isOpen;
     public Animator animator;
     public GameObject obj;
+    public GameObject cardImage;
+    public Sprite sprite; 
     public void OpenChest()
     {
         if (!isOpen)
         {
+            Image im = cardImage.GetComponent<Image>();
+            im.sprite = sprite;
+      
             isOpen = true;
             Debug.Log("Chest  has Oppened");
             animator.SetBool("IsOpen", true);
